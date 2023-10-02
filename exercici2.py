6dde70de06720facc4ca5c718245fe0bff99258b
niky <nikita.barbosa.7e5@itb.cat>
2023-10-02 10:09:31 +0200

    exercici2 mas bonito

--- exercici2.py
+++ exercici2.py
@@ -1,14 +1,16 @@
 # Màquina de venda de bitllets de metro
 
+
 class Ticket:
-    def __init__(self, name:str, price:float):
+    def __init__(self, name: str, price: float):
         self.name = name
         self.price = price
 
-    def __repr__(self): #definir como aparece en la lista
+    def __repr__(self):  # definir como aparece en la lista
         return f"{self.name}"
-    
-def priceCalculator(zone, ticket:Ticket):
+
+
+def priceCalculator(zone, ticket: Ticket):
     print(ticket)
     price = ticket.price
     calculator = 0
@@ -18,37 +20,48 @@ def priceCalculator(zone, ticket:Ticket):
         calculator = 1.35
     elif zone == 3:
         calculator = 1.89
-    result = round(price * calculator,2)
+    result = round(price * calculator, 2)
     print(f"Preu: {result}")
     return result
 
+
 def payment(result):
+    allowedMoney = [0.05, 0.10, 0.20, 0.50, 1, 2, 5, 10, 20, 50]
     moneyGiven = 0
-    
     while moneyGiven < result:
         print("Introdueix l'import a pagar.")
         print(f"Falten: {round(result - moneyGiven, 2)}")
-        moneyGiven += float(input())
-    if moneyGiven >  result:
-        print(f"Canvi: {moneyGiven - result}")
+        input_money = float(input())
+        if input_money in allowedMoney:
+            moneyGiven += input_money
+        else:
+            print(
+                "Aquesta moneda/billet no és vàlida. Si us plau, utilitza només les següents monedes i bitllets:"
+            )
+            print("Monedes: 0.5ct, 1ct, 2ct, 5ct, 10ct, 20ct, 50ct, 1€, 2€")
+            print("Bitllets: 5€, 10€, 20€, 50€")
+
 
 def ticketMachine():
+    metroTickets = [
+        Ticket("Bitllet Senzill", 2.40),
+        Ticket("T-Casual", 11.35),
+        Ticket("T-Usual", 20.00),
+        Ticket("T-Grup", 79.45),
+        Ticket("T-Jove", 40.00),
+    ]
 
-    metroTickets = [Ticket("Bitllet Senzill",2.20),
-                    Ticket("T-Casual", 10.20),
-                    Ticket("T-Mes", 54.00),
-                    Ticket("T-Trimeste", 145.30),
-                    Ticket("T-Jove", 105.00)]
-
-    print("Si us plau, escull el numero del bitllet vols comprar.")
-    for ticket in enumerate(metroTickets):
-        print(ticket)
-    
+    print(
+        "Benvingut a la màquina de venda de tiquets. Si us plau, escull el numero del bitllet que vols comprar."
+    )
+    for idx, ticket in enumerate(metroTickets):
+        print(f"{idx} -> {ticket}")
     userTicketChoice = int(input())
     selectedTicket = metroTickets[userTicketChoice]
-    print(f"Has seleccionat {selectedTicket}. Si us plau, indica quina zona vols a continuacio: 1, 2 o 3")
-    payment(priceCalculator(int(input()),selectedTicket))
-
+    print(
+        f"Has seleccionat {selectedTicket}. Si us plau, indica quina zona vols a continuacio: 1, 2 o 3"
+    )
+    payment(priceCalculator(int(input()), selectedTicket))
 
 
 while True:
