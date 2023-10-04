738acaea93041e006ea556deefd82cd8dfc4a2fd
niky <nikita.barbosa.7e5@itb.cat>
2023-10-04 16:11:23 +0200

    miau

--- II/exercici1.py
+++ II/exercici1.py
@@ -3,7 +3,17 @@ import csv
 
 def csvReader():
     data = csv.reader(open('II/basket_players.csv', 'r'))
-    for i, fila in enumerate(data):
-        print(i,fila)
-    
+    header = next(data)
+    mydict = {columna: [] for columna in header}
+
+    for i, valor in enumerate(data):
+        print(i, valor)
+    print("Files totals: ", i)
+
+    for fila in data:
+        for i, valor in enumerate(fila):
+            columna = header[i]
+            mydict[columna].append(valor) 
+    print(mydict.keys())
 csvReader()
+aa
