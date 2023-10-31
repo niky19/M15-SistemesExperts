36002e802e2f49f0e18ff0bccd7ab92c280120db
niki <nikita.barbosa.7e5@itb.cat>
2023-10-31 09:48:54 +0100

    comit

--- IV/main.py
+++ IV/main.py
@@ -6,7 +6,11 @@ def meteocat():
     data_estacions_detall = csv.reader(open('IV/2022_MeteoCat_Detall_Estacions.csv', 'r'), delimiter=';')
     data_metadades = csv.reader(open('IV/MeteoCat_Metadades.csv', 'r'), delimiter=';')
 
-    dim0 = np.array([[data_estacions, data_estacions_detall],[data_metadades]])
-    print(dim0)
-
+    dim0 = np.array(data_estacions)
+    dim1 = np.array(data_estacions_detall)
+    dim2 = np.array(data_metadades)
+    x = np.array([dim0, dim1, dim2])
+    y = np.array([data_estacions, data_estacions_detall, data_metadades])
+    print(x) 
+    print(y)
 meteocat()
